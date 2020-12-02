9efe9fd4ffdd33d4002a7fe7f41e542ac594f24f
Tim <60073247+djtimo18@users.noreply.github.com>
2020-12-02 09:40:52 +0100

    Ajout des fichiers de base

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+require ('config.php');
+session_start();
+
+
+if (isset($_POST['username']) & isset($_POST['password'])){
+    try {
+        $sth = $conn->prepare("SELECT * FROM user WHERE username=:username");
+        $sth->bindParam(':username', $_POST['username']);
+        $sth->execute();
+        $row = $sth->fetch(PDO::FETCH_ASSOC);
+        $hash = $row['password'];
+        if (password_verify($_POST['password'], $hash)){
+            echo "<a href='index.php'>Allez à votre Dashboard</a>";
+        }else{
+            echo "Mauvais mot de passe our username.";
+        }
+    }catch (PDOException $e){
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+    }
+}else{
+    echo "";
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <form action="" method ="POST">
+        <a>Username</a>
+        <input type="text" placeolder="Username" name="username"></input>
+        <a>Password</a>
+        <input type="password" placeolder="Password" name="password"></input>
+        <button type="submit" >Connexion</button>
+    </form>
+</body>
+</html>
